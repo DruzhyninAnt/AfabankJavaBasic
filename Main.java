89aeb4da8f506916355068e6e93fa45c88c70c58
DruzhyninAnt <78678966+DruzhyninAnt@users.noreply.github.com>
2021-02-07 10:01:28 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.alfalearn.Task3_3;
+
+public class Main {
+    public static void main(String[] args) {
+
+        double price=830.0;
+        double newPrice=830.0*(1-0.13);
+
+        System.out.println(newPrice);
+
+        System.out.println("Last price: "+newPrice*1.07);
+
+    }
+}
