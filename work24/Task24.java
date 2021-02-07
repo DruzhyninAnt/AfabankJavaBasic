89ef517545a6651629abd7259ac168d2ed708880
DruzhyninAnt <78678966+DruzhyninAnt@users.noreply.github.com>
2021-02-07 21:24:33 +0200

    Update Task24.java

--- work24/Task24.java
+++ work24/Task24.java
@@ -6,13 +6,15 @@ public class Task24 {
     public static void main(String[] args) {
 
         int number = 547;
-     //   Способ 1 (только для трехзначных):
-            int ed = number % 10;
+    
+        //   Способ 1 (только для трехзначных):
+          
+        int ed = number % 10;
              int des = number / 10 % 10;
                 int sotni = number / 100;
              int res = ed * 100 + des * 10 + sotni;
 
-        System.out.println("Способ 1 (только для трехзначных):"+number + "-" + res + "=" + (number - res));
+        System.out.println("По способу 1 (только для трехзначных):"+number + "-" + res + "=" + (number - res));
 
 
         //   Способ 2 (для любых чисел без циклов) :
@@ -20,7 +22,8 @@ public class Task24 {
              StringBuffer stringBuilder = new StringBuffer(String.valueOf(number));
 
                 int ch1 = Integer.parseInt(stringBuilder.reverse().toString());
-        System.out.println("Способ 2 (для любых чисел и без циклов) :"+number + "-" + ch1 + "=" + (number - ch1));
+        
+        System.out.println("По способу 2 (для любых чисел) :"+number + "-" + ch1 + "=" + (number - ch1));
 
 
     }}
